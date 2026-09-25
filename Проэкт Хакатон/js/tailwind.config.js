562d3bf032b8c61c5d0d21ff0e37d6301033f509
denis100strike <denis100strike@gmail.com>
2026-09-25 21:47:50 +0300

    Add files via upload

---
+++ Проэкт Хакатон/js/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'system-ui', 'sans-serif'],
+        mono: ['"JetBrains Mono"', 'monospace'],
+      },
+      colors: {
+        profi: {
+          green: '#009640',
+          yellow: '#FFDE00',
+          dark: '#0B1315',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
